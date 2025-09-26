8bded8ab5d351fafc3c314708c67b7ebfc97602b
dDiazcL <diea.diaz@duocuc.cl>
2025-09-26 00:10:06 -0300

    Commit: Fix Pagina de Recuperacion de contraseña

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { Ui } from 'src/app/services/ui';
 
 @Component({
@@ -11,12 +12,33 @@ import { Ui } from 'src/app/services/ui';
 export class ResetPasswordPage {
   email: string = '';
 
-  constructor(private router: Router, private ui: Ui) {}
+  constructor(private router: Router, private ui: Ui, private toastController: ToastController) {}
 
-  resetPassword() {
-    if (this.email) {
+  async presentToast(message: string, color: string = 'danger') {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      color,
+      position: 'bottom'
+    });
+    await toast.present();
+  }
+
+  async resetPassword() {
+    if (!this.email) {
+      this.presentToast('Por favor ingresa tu correo 📧')
+      return;
+      }
+
+      const emailVal = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailVal.test(this.email)) {
+        this.presentToast('Ingresa un correo válido 📬');
+        return;
+      }
+
+      this.presentToast('Se envió el enlace a tu correo ✅', 'success');
+
       this.ui.blurActiveElement();
-      this.router.navigate(['/login']);
-    }
+      this.router.navigate(['/login'])
   }
 }
